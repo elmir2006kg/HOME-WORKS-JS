9aeb7a40ab830a2c72c2c7d9f3d4c9a11e0bc018
elmir <elmirbek2000kg>
2025-02-15 21:31:09 +0300

    save

--- main.js
+++ main.js
@@ -1,93 +1,116 @@
-const button = document.querySelector("button");
+1 - task;
+
+const ptag = document.querySelector(".hello");
+
+setTimeout(() => {
+  ptag.textContent = "Ураа";
+}, 10000);
+
+task2;
+
+let y;
+const div = document.querySelector("div");
+
+function changercolor() {
+  y = Math.floor(Math.random() * 12852);
+}
+let r = setInterval(() => {
+  changercolor();
+  div.style.backgroundColor = "#" + y;
+}, 1000);
+
+setTimeout(() => {
+  clearTimeout(r);
+}, 60000);
+
+task - 3;
+
 const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+const ptag2 = document.querySelector(".ptag2");
+const button = document.querySelector(".add");
 
-let Arra = [];
+button.addEventListener("click", () => {
+  setInterval(() => {
+    if (inputvalue < 0) return;
+    ptag2.textContent = inputvalue--;
+  }, 1000);
 
-button.addEventListener("click", (event) => {
-  event.preventDefault();
-  const inputvalue = input.value;
-  if (!inputvalue) {
-    alert("input is empty");
-  } else {
-    const a = {
-      id: Date.now().toString(),
-      value: inputvalue,
-    };
-    Arra.push(a);
-    render(Arra);
-    input.value = "";
-  }
-  window.addEventListener("offline", () => {
-    localStorage.setItem("array", JSON.stringify(Arra));
-  });
-
-  window.addEventListener("beforeupload", () => {
-    window.addEventListener("online", () => {
-      localStorage.getItem(JSON.parse(Arra));
-    });
-  });
+  let inputvalue = input.value;
+  input.value = "";
 });
 
-function render(data) {
-  ul.innerHTML = "";
-  data.map((data) => {
-    const delet = document.createElement("button");
-    const li = document.createElement("li");
-    const ptag = document.createElement("p");
-    const divin = document.createElement("div");
-    const divin2 = document.createElement("div");
-    const inputin = document.createElement("input");
-    const modalofcontainer = document.querySelector(".modalofcontainer");
-    const button1 = document.querySelector(".button1");
-    const button2 = document.querySelector(".button2");
+task - 4;
 
-    inputin.type = "checkbox";
-    divin.className = "innerdiv";
-    divin2.className = "innerdiv2";
-    delet.className = "delete ";
-    ptag.className = "ptag";
+const input1 = document.querySelector(".input1");
+let inputvalue1;
+const span1 = document.querySelector(".span2");
+const span2 = document.querySelector(".span1");
+const twopoint = document.querySelector(".twopoint");
+const start = document.querySelector(".start");
+const pause = document.querySelector(".pause");
+let a;
 
-    ptag.textContent = data.value;
-    delet.textContent = "delete";
+input1.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    span2.style.display = "inline";
+    twopoint.style.display = "inline";
 
-    // ////
-    inputin.addEventListener("change", () => {
-      setTimeout(() => {
-        modalofcontainer.style.display = "block";
-      }, 500);
-      if (inputin.checked) {
-        ptag.style.textDecorationLine = "line-through";
-        ptag.style.textDecorationColor = " black";
+    inputvalue1 = input1.value;
+    span1.textContent = inputvalue1;
+    let r = input3.value;
 
-        button1.addEventListener("click", () => {
-          doo(data.id);
-          modalofcontainer.style.display = "none";
-        });
-        button2.addEventListener("click", () => {
-          modalofcontainer.style.display = "none";
-        });
-      } else {
-        ptag.style.textDecorationLine = "none";
-        ptag.style.textDecorationColor = "none";
-        modalofcontainer.style.display = "none";
-      }
+    if (inputvalue1 >= 9) {
+      span2.style.display = "none";
+    }
+    start.addEventListener("click", () => {
+      pause.style.display = "inline";
+      start.style.display = "none";
+      span3.textContent = inputvalue--;
+
+      pause.addEventListener("click", () => {
+        start.style.display = "inline";
+        pause.style.display = "none";
+        clearInterval(a);
+        clearInterval(b);
+      });
+
+      a = setInterval(() => {
+        if (inputvalue1 < 0) return;
+
+        span1.textContent = inputvalue1--;
+      }, 60000);
     });
-    ////
-    divin2.append(inputin, delet);
-    divin.append(ptag, divin2);
-    li.appendChild(divin);
-    ul.appendChild(li);
+  }
+});
 
-    delet.addEventListener("click", () => {
-      doo(data.id);
+const input3 = document.querySelector(".input2");
+const span3 = document.querySelector(".span3");
+const restart = document.querySelector(".restart");
+let inputvalue;
+let b;
+input3.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    inputvalue = input3.value;
+    span3.textContent = inputvalue;
+
+    start.addEventListener("click", () => {
+      b = setInterval(() => {
+        if (inputvalue < 1) {
+          if (inputvalue1 < 0) return;
+          span1.textContent = inputvalue1--;
+          inputvalue = 59;
+        }
+
+        if (inputvalue < 0) return;
+
+        span3.textContent = inputvalue--;
+      }, 1000);
+      restart.addEventListener("click", () => {
+        span1.textContent = inputvalue1;
+        let t = input3.value;
+        inputvalue = t;
+        span3.textContent = inputvalue--;
+      });
     });
-  });
-}
-
-function doo(id) {
-  Arra = Arra.filter((item) => {
-    return item.id !== id;
-  });
-  render(Arra);
-}
+  }
+});
